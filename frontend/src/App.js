34672e575b3f4af50de64ee7554d850fb40ef6b5
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-09-20 18:52:49 -0500

    Added the productscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header'
-import { BrouserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
